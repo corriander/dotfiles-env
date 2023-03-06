fe1b88647f7d2e168e534f20aeb3ef5c16cbc032
Alex Corrie <ajccode@gmail.com>
2023-03-06 09:32:06 +0000

    tweak cross-platform profile
    Some issues with the logic working everywhere

--- .config/profile.macos.sh
+++ .config/profile.macos.sh
@@ -1,2 +1,4 @@
 add_to_path /opt/homebrew/bin
 add_to_path $(brew --prefix)/opt/openjdk/bin
+
+export EDITOR=/opt/homebrew/bin/nvim
---
+++ .config/profile.macos.zsh
@@ -0,0 +1,4 @@
+# Nothing specific to macOS & zsh currently but better to be explicit
+# as there's no easy way to optionally load the profile easily without
+# errors when it doesn't exist, but show errors within it. A log would
+# help.
