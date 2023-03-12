f070bd630baf4d07315ede400bbba91c902687f1
Alex Corrie <ajccode@gmail.com>
2023-03-12 20:28:49 +0000

    add autoreload to ipython config

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -1,1 +1,12 @@
 c.TerminalInteractiveShell.editing_mode = 'vi'
+c.TerminalInteractiveShell.autoformatter = 'black'
+
+c.InteractiveShellApp.extensions = [
+	'autoreload'
+]
+
+c.InteractiveShellApp.exec_lines = [
+	"%autoreload 3",
+	"print('Warning: autoreload 3 is enabled in ipython_config.py; disable to improve performance.')"
+]
+
