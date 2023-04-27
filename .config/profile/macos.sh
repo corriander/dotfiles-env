3016e94c3b11eac666b4eeddbe30298429cf7b09
Alex Corrie <ajccode@gmail.com>
2023-04-27 11:42:12 +0100

    set vim default on wsl but nvim on macos (temp)

--- .config/profile/macos.sh
+++ .config/profile/macos.sh
@@ -2,3 +2,8 @@ add_to_path /opt/homebrew/bin
 add_to_path $(brew --prefix)/opt/openjdk/bin
 
 export EDITOR=/opt/homebrew/bin/nvim
+
+# aliases
+function vim () {
+	nvim $*
+}
--- .config/profile/wsl.sh
+++ .config/profile/wsl.sh
@@ -4,3 +4,6 @@
 # https://esc.sh/blog/ssh-agent-windows10-wsl2/
 /usr/bin/keychain -q --nogui $HOME/.ssh/id_ed25519
 . $HOME/.keychain/$HOST-sh
+
+# TODO shared vim/neovim config needs some work. nvim undo files break vim
+export EDITOR=/usr/bin/vim
