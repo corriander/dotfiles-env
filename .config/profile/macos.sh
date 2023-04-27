2fb6e1234b076acde585a8d1476e9d4ea6fbbe89
Alex Corrie <ajccode@gmail.com>
2023-04-27 13:40:43 +0100

    add gnu sed, gnu getopt to path on macos

--- .config/profile/macos.sh
+++ .config/profile/macos.sh
@@ -1,6 +1,7 @@
 add_to_path /opt/homebrew/bin
 add_to_path $(brew --prefix)/opt/openjdk/bin
-
+add_to_path $(brew --prefix)/opt/gnu-sed/libexec/gnubin
+add_to_path $(brew --prefix)/opt/gnu-getopt/bin
 export EDITOR=/opt/homebrew/bin/nvim
 
 # aliases
