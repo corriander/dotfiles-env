f5ae63366783a992c2c73a980639d24ea4c17e11
Alex Corrie <ajccode@gmail.com>
2023-08-25 14:01:41 +0100

    feat: add more terminal colours and use monokai

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -20,7 +20,20 @@ end
 -- config.color_scheme = 'Jellybeans (Gogh)'
 -- config.color_scheme = 'Kanagawa (Gogh)'
 -- config.color_scheme = 'Kasugano (terminal.sexy)'
-config.color_scheme = 'Embers (dark) (terminal.sexy)'
+-- config.color_scheme = 'Embers (dark) (terminal.sexy)'
+-- config.color_scheme = 'Hybrid'
+-- config.color_scheme = 'rose-pine'
+-- config.color_scheme = 'Azu (Gogh)'
+-- config.color_scheme = 'Smyck'
+-- config.color_scheme = 'Lost Woods (terminal.sexy)'
+config.color_scheme = 'Monokai Dark (Gogh)'
+-- config.color_scheme = 'Operator Mono Dark'
+-- config.color_scheme = 'Red Planet'
+-- config.color_scheme = 'MaterialDark (Gogh)'
+-- config.color_scheme = 'Numix Darkest (terminal.sexy)'
+-- config.color_scheme = 'zenburn (terminal.sexy)'
+
+
 
 config.window_background_opacity = 0.90
 
