24280a0cf52f968fba500f5560ff09c82745ede7
Alex Corrie <ajccode@gmail.com>
2023-10-15 11:59:35 +0100

    feat: add wsl-specific workaround for granted
    Assuming a profile doesn't work correctly in wsl and there's not really
    an alternative tool. This workaround retains the ability to open up a
    containerised console but takes over the assume command for cli by
    setting AWS_PROFILE, using aws sso login, and clearing down on --unset.

--- .config/profile/wsl.zsh
+++ .config/profile/wsl.zsh
@@ -5,3 +5,29 @@
 SPACESHIP_ASYNC_SHOW=false
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+assume () {
+    cmd=/usr/local/bin/assume
+    if [[ "$1" == "--unset" ]]; then
+        $cmd "$@" && unset AWS_PROFILE || return $?
+        return 0
+    fi
+
+    aws_profiles=($(aws configure list-profiles))
+    arg1_is_profile=false
+    for item in "${aws_profiles[@]}"; do
+        [[ "$item" == "$1" ]] && arg1_is_profile=true && break
+    done
+
+    # if arg1_is_profile; hijack the command and do our own thing on WSL because it's broken
+    if $arg1_is_profile; then
+        echo >&2 "$(tput setaf 3)Overriding granted/assume (is it still broken on WSL?)$(tput op)"
+        echo ""
+        export AWS_PROFILE=$1
+        aws sso login
+    else
+        $cmd "$@"
+    fi
+
+    return 0
+}
