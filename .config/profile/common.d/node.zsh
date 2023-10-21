40cbd2f5044297382eea118f6d994f9ca5d30720
Alex Corrie <ajccode@gmail.com>
2023-10-21 12:12:06 +0100

    feat: move nvm dir & add default to env

--- .config/profile/common.d/node.zsh
+++ .config/profile/common.d/node.zsh
@@ -1,2 +1,13 @@
-export NVM_DIR="$HOME/.config/nvm"
+export NVM_DIR="$HOME/app/nvm"
 # node install / management handled via zsh nvm plugin
+
+_get_default_node() {
+    # inspect nvm install dir for default node path
+    default_lts_name=$(<$NVM_DIR/alias/lts/'*') 2>/dev/null
+    default_lts_version=$(<$NVM_DIR/alias/$default_lts_name) 2>/dev/null
+    echo $NVM_DIR/versions/node/${default_lts_version}/bin/node
+}
+
+export MY_NODE_DEFAULT=$(_get_default_node)
+
+add_to_path $(dirname $MY_NODE_DEFAULT) after
