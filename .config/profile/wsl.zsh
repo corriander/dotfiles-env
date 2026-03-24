866fff31d1e2cdf00b089c37aff6d383d163bb4f
Alex Corrie <ajccode@gmail.com>
2026-03-24 23:18:57 +0000

    fix: docker compinit noise on wsl
    .zshenv skip is needed to be early enough in shell source

--- .config/profile/wsl.zsh
+++ .config/profile/wsl.zsh
@@ -4,6 +4,23 @@
 # https://github.com/spaceship-prompt/spaceship-prompt/issues/1193#issuecomment-1432980561
 SPACESHIP_ASYNC_SHOW=false
 
+# Docker Desktop on WSL can leave a dangling _docker completion symlink when
+# the Desktop VM is down. Mirror vendor completions locally and drop broken
+# links so compinit still succeeds.
+_zsh_vendor_completions=/usr/share/zsh/vendor-completions
+_zsh_vendor_cache=${XDG_CACHE_HOME:-$HOME/.cache}/zsh/vendor-completions
+
+if [[ -L $_zsh_vendor_completions/_docker && ! -e $_zsh_vendor_completions/_docker ]]; then
+    mkdir -p "$_zsh_vendor_cache"
+    for _completion in $_zsh_vendor_completions/*(N); do
+        [[ -L $_completion && ! -e $_completion ]] && continue
+        ln -sfn "$_completion" "$_zsh_vendor_cache/${_completion:t}"
+    done
+    fpath=($_zsh_vendor_cache ${fpath:#$_zsh_vendor_completions})
+fi
+
+unset _completion _zsh_vendor_cache _zsh_vendor_completions
+
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
 
 __configure_aws_codeartifact () {
---
+++ .zshenv
@@ -0,0 +1,3 @@
+# OMZ takes care of this; on WSL if we don't skip we can trigger errors on shell start
+# e.g. with ephemeral docker completions
+skip_global_compinit=1
